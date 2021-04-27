f7c5a2a3f540d141421d7ffbe73c948a347158b4
Alastair Odhiambo <alastairodhiambo@outlook.com>
2021-04-26 20:59:15 -0400

    set loading and validity checks

--- src/components/Model/Model.js
+++ src/components/Model/Model.js
@@ -7,6 +7,7 @@ import "@google/model-viewer";
 export default function Model() {
   const [image, setImage] = useState("");
   const [modelName, setModelName] = useState("");
+  const [valid, setValid] = useState(null);
 
   const match = useRouteMatch("/model/:id");
   const model = match.params.id;
@@ -16,26 +17,48 @@ export default function Model() {
       .collection("models")
       .doc(model)
       .onSnapshot((doc) => {
-        setImage(doc.data().image || "");
-        setModelName(doc.data().name);
+        if (doc.exists) {
+          setImage(doc.data()?.image || "");
+          setModelName(doc.data()?.name);
+          setValid(true);
+        } else {
+          setValid(false);
+        }
       });
+    // setLoading(false);
     return unmount;
   }, []);
 
+  if (valid) {
+    return (
+      <section className="content">
+        <h1>{modelName}</h1>
+        <div className="model-view">
+          <img src={image} alt={image} />
+          {/* <div className="model">
+              <model-viewer
+                src={image}
+                camera-controls
+                shadow-intensity="1"
+              ></model-viewer>
+            </div> */}
+        </div>
+        <EditModel currentModel={model} />
+      </section>
+    );
+  }
+
+  if (valid === false) {
+    return (
+      <>
+        <p>Unable to find the model...</p>
+      </>
+    );
+  }
+
   return (
-    <section className="content">
-      <h1>{modelName}</h1>
-      <div className="model-view">
-        <img src={image} alt={image} />
-        {/* <div className="model">
-            <model-viewer
-              src={image}
-              camera-controls
-              shadow-intensity="1"
-            ></model-viewer>
-          </div> */}
-      </div>
-      <EditModel currentModel={model} />
-    </section>
+    <>
+      <p>Loading...</p>
+    </>
   );
 }
