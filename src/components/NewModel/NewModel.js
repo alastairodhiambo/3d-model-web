8974d515dfab6249d192b4e30354f4cc413300f4
Alastair Odhiambo <alastairodhiambo@outlook.com>
2021-04-27 07:28:38 -0400

    implementing the add model file functionality

--- src/components/NewModel/NewModel.js
+++ src/components/NewModel/NewModel.js
@@ -2,10 +2,14 @@ import React, { useState } from "react";
 import { db, storage } from "../../firebase";
 import { useHistory } from "react-router-dom";
 
+import "./NewModel.scss";
+
 export default function NewModelForm() {
   const [modelName, setModelName] = useState("");
   const [file, setFile] = useState(null);
   const [fileRef, setFileRef] = useState(null);
+  const [modelFile, setModelFile] = useState(null);
+  const [modelFileRef, setModelFileRef] = useState(null);
   const [loading, setLoading] = useState(false);
   let history = useHistory();
 
@@ -16,8 +20,13 @@ export default function NewModelForm() {
   };
 
   const onFileChange = async (e) => {
-    setFile(e.target.files[0]);
-    setFileRef(storageRef.child(e.target.files[0].name));
+    setFile(e.target?.files[0]);
+    setFileRef(storageRef.child(e.target?.files[0].name));
+  };
+
+  const onModelFileChange = async (e) => {
+    setModelFile(e.target?.files[0]);
+    setModelFileRef(storageRef.child(e.target?.files[0].name));
   };
 
   const onModelCreate = async () => {
@@ -27,12 +36,14 @@ export default function NewModelForm() {
     setLoading(true);
 
     await fileRef.put(file);
+    await modelFileRef.put(modelFile);
 
     db.collection("models")
       .doc()
       .set({
         name: modelName,
         image: await fileRef.getDownloadURL(),
+        model: await modelFileRef.getDownloadURL(),
       });
     setModelName("");
     setLoading(false);
@@ -59,7 +70,12 @@ export default function NewModelForm() {
         <input value={modelName} onChange={onModelNameChange} type="text" />
       </div>
       <div>
-        <input type="file" onChange={onFileChange} />
+        <label htmlFor="image-file">Image File</label>
+        <input id="image-file" type="file" onChange={onFileChange} />
+      </div>
+      <div>
+        <label htmlFor="model-file">Model File</label>
+        <input id="model-file" type="file" onChange={onModelFileChange} />
       </div>
       <button id="create-model" onClick={onModelCreate}>
         Create model
