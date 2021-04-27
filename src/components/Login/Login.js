0469c481a25572c85894f3580ca1ba6ca284a038
Alastair Odhiambo <alastairodhiambo@outlook.com>
2021-04-27 07:24:21 -0400

    update Login styling

--- src/components/Login/Login.js
+++ src/components/Login/Login.js
@@ -1,8 +1,10 @@
 import { useRef, useState } from "react";
-import { Form, Button, Card, Alert } from "react-bootstrap";
+import { Container, Form, Button, Card, Alert } from "react-bootstrap";
 import { Link, useHistory } from "react-router-dom";
 
 import { useAuth } from "../../contexts/AuthContext";
+
+import "./Login.scss";
 
 export default function Login() {
   const emailRef = useRef();
@@ -20,7 +22,7 @@ export default function Login() {
       setLoading(true);
       await login(emailRef.current.value, passwordRef.current.value);
       setLoading(false);
-      history.push("/");
+      history.push("/dashboard");
     } catch (e) {
       setError("Failed to log in");
       console.error(e);
@@ -29,41 +31,43 @@ export default function Login() {
 
   return (
     <>
-      <Card>
-        <Card.Body>
-          <h2 className="text-center mb-4">Log In</h2>
-          {error && <Alert variant="danger">{error}</Alert>}
-          <Form onSubmit={handleSubmit}>
-            <Form.Group id="email">
-              <Form.Label>Email</Form.Label>
-              <Form.Control
-                type="email"
-                ref={emailRef}
-                required
-                autoComplete="off"
-              />
-            </Form.Group>
-            <Form.Group id="password">
-              <Form.Label>Password</Form.Label>
-              <Form.Control
-                type="password"
-                ref={passwordRef}
-                required
-                autoComplete="off"
-              />
-            </Form.Group>
-            <Button disabled={loading} className="w-100" type="submit">
-              Log In
-            </Button>
-          </Form>
-          <div className="w-100 text-center mt-3">
-            <Link to="/forgot-password">Forgot Password</Link>
+      <section className="container">
+        <Card className="w-100" style={{ minWidth: "400px" }}>
+          <Card.Body>
+            <h2 className="text-center mb-4">Log In</h2>
+            {error && <Alert variant="danger">{error}</Alert>}
+            <Form onSubmit={handleSubmit}>
+              <Form.Group id="email">
+                <Form.Label>Email</Form.Label>
+                <Form.Control
+                  type="email"
+                  ref={emailRef}
+                  required
+                  autoComplete="off"
+                />
+              </Form.Group>
+              <Form.Group id="password">
+                <Form.Label>Password</Form.Label>
+                <Form.Control
+                  type="password"
+                  ref={passwordRef}
+                  required
+                  autoComplete="off"
+                />
+              </Form.Group>
+              <button disabled={loading} type="submit">
+                Log In
+              </button>
+            </Form>
+            <div className="w-100 text-center mt-3">
+              <Link to="/forgot-password">Forgot Password</Link>
+            </div>
+          </Card.Body>
+          <div className="w-100 text-center mt-2 pb-4">
+            Need an account? <Link to="/signup">Sign Up</Link>
           </div>
-        </Card.Body>
-      </Card>
-      <div className="w-100 text-center mt-2">
-        Need an account? <Link to="/signup">Sign Up</Link>
-      </div>
+        </Card>
+      </section>
     </>
   );
 }
