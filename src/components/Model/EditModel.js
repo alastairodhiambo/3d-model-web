bc3d5651e8b30b2f3285ef549cc6f1d9c9ca33f0
Alastair Odhiambo <alastairodhiambo@outlook.com>
2021-04-27 07:27:55 -0400

    Adding the uploading and editing functionality

--- src/components/Model/EditModel.js
+++ src/components/Model/EditModel.js
@@ -2,10 +2,12 @@ import React, { useState } from "react";
 import { useHistory } from "react-router-dom";
 import { db, storage } from "../../firebase";
 
-export default function UploadModel({ currentModel }) {
+export default function UploadModel({ currentModel, name }) {
   const [file, setFile] = useState(null);
-  const [modelName, setModelName] = useState("");
+  const [modelFile, setModelFile] = useState(null);
+  const [modelName, setModelName] = useState(name);
   let history = useHistory();
+  const storageRef = storage.ref().child("models");
 
   /****  Updating the model name  ****/
   const onModelNameChange = (e) => {
@@ -13,29 +15,50 @@ export default function UploadModel({ currentModel }) {
   };
 
   const onUpdateModelName = () => {
-    if (!modelName) {
+    if (!modelName || modelName === name) {
       return;
     }
+    console.log("ran");
     db.collection("models").doc(currentModel).update({
       name: modelName,
     });
-    setModelName("");
   };
   /***********/
 
-  /****  Uploading the file  ****/
+  /****  Uploading the image file  ****/
   const onFileChange = (e) => {
     setFile(e.target.files[0]);
   };
 
   const onUpload = async () => {
-    const storageRef = storage.ref().child("models");
+    if (!file) {
+      return;
+    }
     const fileRef = storageRef.child(file.name);
     await fileRef.put(file);
     db.collection("models")
       .doc(currentModel)
       .update({
         image: await fileRef.getDownloadURL(),
+      });
+  };
+  /***********/
+
+  /****  Uploading the model file  ****/
+  const onModelFileChange = (e) => {
+    setModelFile(e.target.files[0]);
+  };
+
+  const onModelUpload = async () => {
+    if (!modelFile) {
+      return;
+    }
+    const modelFileRef = storageRef.child(file?.name);
+    await modelFileRef.put(modelFile);
+    db.collection("models")
+      .doc(currentModel)
+      .update({
+        model: await modelFileRef.getDownloadURL(),
       });
   };
   /***********/
@@ -56,6 +79,10 @@ export default function UploadModel({ currentModel }) {
       <div>
         <input type="file" onChange={onFileChange} />
         <button onClick={onUpload}>Upload image</button>
+      </div>
+      <div>
+        <input type="file" onChange={onModelFileChange} />
+        <button onClick={onModelUpload}>Upload model</button>
       </div>
       <div>
         <button onClick={onDelete}>Delete Model</button>
